c71253494ea81da6fb9aa11b6ecaf8cd2f756f81
Skliton5 <sk.litonn79@email.com>
2024-11-15 17:06:07 +0530

    Content yo

--- Yo.js
+++ Yo.js
@@ -2,3 +2,5 @@ var yq = "Welcome to Spito velly";
 var yop = "Welcome to Nubra Velly";
 console.log(yq);
 console.log(yop);
+var cv = 2002;
+console.log(cv);
--- Yo.ts
+++ Yo.ts
@@ -2,3 +2,5 @@ var yq:any="Welcome to Spito velly";
 var yop:any="Welcome to Nubra Velly";
 console.log(yq);
 console.log(yop);
+var cv:number=2002;
+console.log(cv);
